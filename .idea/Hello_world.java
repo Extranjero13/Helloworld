417f14fdf069f7603edce7a5df93a69d5b507ff9
Extranjero <arivera18@rockets.utoledo.edu>
2023-09-15 17:22:54 -0400

    Assignment one
    New test

---
+++ .idea/Hello_world.java
@@ -0,0 +1,8 @@
+public class Helloworld {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello World!");
+
+    }
+}
